eef572b49b0848e24d9fae6891bed30ae5e64cd9
D. Ajay Kumar <154305661+ajay70aa@users.noreply.github.com>
2025-04-29 17:17:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-//your JS code here. If required.
+let currentAudio = null;
+
+document.querySelectorAll('.btn').forEach(button => {
+  button.addEventListener('click', () => {
+    if (currentAudio) {
+      currentAudio.pause();
+      currentAudio.currentTime = 0;
+    }
+
+    const soundPath = button.getAttribute('data-sound');
+    currentAudio = new Audio(soundPath);
+    currentAudio.play();
+  });
+});
+
+const stopBtn = document.querySelector('.stop');
+stopBtn.addEventListener('click', () => {
+  if (currentAudio) {
+    currentAudio.pause();
+    currentAudio.currentTime = 0;
+  }
+});
